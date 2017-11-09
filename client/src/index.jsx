9119b7671891a3364e714e0c854928413f8076c1
Andrew Chen <andrewchen.c44@gmail.com>
2017-11-08 19:07:26 -0800

    Implemented git api helper function

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -15,7 +15,7 @@ class App extends React.Component {
 
   search (term) {
     console.log(`${term} was searched`);
-    // TODO
+    getReposByUsername(`${term}`);
   }
 
   render () {
--- helpers/github.js
+++ helpers/github.js
@@ -8,15 +8,19 @@ let getReposByUsername = (handle) => {
   // The options object has been provided to help you out, 
   // but you'll have to fill in the URL
   let options = {
-    url: "https://api.github.com",
+    url: `https://api.github.com/users/${handle}/repos`,
+    method: 'GET',
     headers: {
       'User-Agent': 'request',
       'Authorization': `token ${config.TOKEN}`
     }
   };
-
+  request(options, function(err, res, body){
+    if(err) {console.log('there is an error', err)}
+    console.log('data', JSON.parse(body));
+  })
 }
 
-getReposByUsername('andrewchen.c44@gmail.com');
+getReposByUsername('andrewchen44');
 
 module.exports.getReposByUsername = getReposByUsername;
